877aa0984e7f57ad84a234fd6fbf99cd02747aa7
Thibault Gérondal <thibault.gerondal@sortlist.com>
2022-07-19 15:25:00 +0200

    bash fix
    Signed-off-by: Thibault Gérondal <thibault.gerondal@sortlist.com>

--- version.sh
+++ version.sh
@@ -1,33 +1,39 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 
-GIT_LIST_TAGS_COMMAND=${GIT_LIST_TAGS_COMMAND:-"git ls-remote --tags --refs -q origin"}
+if [[ -z "$GIT_LIST_TAGS_COMMAND" ]]; then
+  GIT_LIST_TAGS_COMMAND=( "git" "ls-remote" "--tags" "--refs" "-q" "origin" )
+else
+  IFS=' ' read -r -a GIT_LIST_TAGS_COMMAND <<<"$GIT_LIST_TAGS_COMMAND"
+fi
 
-function base_version() {
-  echo $(date +%G.%-V)
+base_version() {
+  date +%G.%-V
 }
 
-function compute_version() {
-  local prefix=$1
-  local prerelease=$2
-  local preprefix=$3
+compute_version() {
+  local prefix prerelease preprefix next inc rc
 
-  local next="${prefix}$(base_version)"
-  local inc=$($GIT_LIST_TAGS_COMMAND "${next}.*" | grep -v '\-.*[0-9]\+$' --count ||:)
-  inc=$(($inc+1))
+  prefix=$1
+  prerelease=$2
+  preprefix=$3
+
+  next="${prefix}$(base_version)"
+  inc=$("${GIT_LIST_TAGS_COMMAND[@]}" "${next}.*" | grep -v '\-.*[0-9]\+$' --count ||:)
+  inc=$((inc+1))
 
   if [ "$prerelease" = true ]
   then
     next="${next}.${inc}"
-    rc=$($GIT_LIST_TAGS_COMMAND "${next}-${preprefix}*" | wc -l)
-    rc=$(($rc+1))
+    rc=$("${GIT_LIST_TAGS_COMMAND[@]}" "${next}-${preprefix}*" | wc -l)
+    rc=$((rc+1))
     echo "${next}-${preprefix}${rc}"
   else
     echo "${next}.${inc}"
   fi
 }
 
-version="$(compute_version $1 $2 $3)"
+version="$(compute_version "$1" "$2" "$3")"
 
 echo "::set-output name=version::$version"
